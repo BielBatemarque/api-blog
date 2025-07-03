6446aaf9a150d61a879271c4a14b440572675f1c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-07-03 16:24:02 -0300

    feat: funcionalidades controller e service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthControler {
+    constructor(private readonly authService: AuthService){}
+
     @Post('login')
-    login() {}
+    login() {
+        return this.authService.doLogin();
+    }
 }
