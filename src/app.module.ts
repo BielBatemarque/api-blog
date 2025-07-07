147a8a024f894e758808bf42e06548f8a34a88ab
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-07-07 08:38:54 -0300

    feat: conexão banco de dados postgree TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,31 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          type: 'postgres',
+          host: 'localhost', //colocar em variaveis de ambiente
+          port: 5430,
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB,
+          synchronize: process.env.DB_SYNCHRONIZE === '1',
+          autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1'
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
