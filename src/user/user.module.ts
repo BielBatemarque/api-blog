468e0ad3b475aae88dab4e46d94fae6e7cdfad61
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-07-07 08:50:17 -0300

    Feat: criando entity de user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
   controllers: [UserController],
 })
